c01fd29c44bf33b522f2aa8ddfc186a2bbd02bfe
WaelOuederni <wael.ouederni@esprit.tn>
2017-04-14 01:25:26 +0200

    Add files via upload

---
+++ config/auth.js
@@ -0,0 +1,3 @@
+/**
+ * Created by WAEL on 13/04/2017.
+ */
---
+++ config/database.js
@@ -0,0 +1,9 @@
+/**
+ * Created by WAEL on 13/04/2017.
+ */
+
+module.exports = {
+
+    'url' : 'mongodb://admin:admin@ds123050.mlab.com:23050/testfb'
+
+};
